3ea7bb104410e7ac4476e7edf0fc2c6df6155333
전민오 <jbj338033@gmail.com>
2025-07-30 20:53:20 +0900

    unity

--- src/main/kotlin/com/comjeonggosi/domain/user/presentation/controller/UserController.kt
+++ src/main/kotlin/com/comjeonggosi/domain/user/presentation/controller/UserController.kt
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/users")
-class UserController(private val userService: UserService) {
+class UserController(
+    private val userService: UserService
+) {
     @GetMapping("/my")
     suspend fun getMyUser() = userService.getMyUser()
 }
