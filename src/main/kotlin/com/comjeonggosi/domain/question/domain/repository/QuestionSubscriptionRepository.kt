bab3a54f09af8a94963debb57a169664060bc4e3
전민오 <jbj338033@gmail.com>
2025-08-17 17:51:18 +0900

    feat: question subscription domain

--- src/main/kotlin/com/comjeonggosi/domain/question/domain/repository/QuestionSubscriptionRepository.kt
+++ src/main/kotlin/com/comjeonggosi/domain/question/domain/repository/QuestionSubscriptionRepository.kt
@@ -1,12 +1,9 @@
 package com.comjeonggosi.domain.question.domain.repository
 
 import com.comjeonggosi.domain.question.domain.entity.QuestionSubscriptionEntity
-import kotlinx.coroutines.flow.Flow
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
 interface QuestionSubscriptionRepository: CoroutineCrudRepository<QuestionSubscriptionEntity, Long> {
     suspend fun findByUserId(userId: Long): QuestionSubscriptionEntity?
-    suspend fun existsByUserId(userId: Long): Boolean
-
-    fun findAllByHourAndMinute(hour: Int, minute: Int): Flow<QuestionSubscriptionEntity>
+    suspend fun findAllByHour(hour: Int): List<QuestionSubscriptionEntity>
 }
