84259a110bfacabad1e4208a5ad88c9a97b231f4
su080915 <tbr335@naver.com>
2025-08-21 17:48:34 +0900

    feat: connect articleId

--- src/main/kotlin/com/comjeonggosi/domain/admin/quiz/application/service/AdminQuizService.kt
+++ src/main/kotlin/com/comjeonggosi/domain/admin/quiz/application/service/AdminQuizService.kt
@@ -3,6 +3,8 @@ package com.comjeonggosi.domain.admin.quiz.application.service
 import com.comjeonggosi.common.exception.CustomException
 import com.comjeonggosi.domain.admin.quiz.presentation.request.dto.CreateQuizRequest
 import com.comjeonggosi.domain.admin.quiz.presentation.request.dto.UpdateQuizRequest
+import com.comjeonggosi.domain.article.domain.error.ArticleErrorCode
+import com.comjeonggosi.domain.article.domain.repository.ArticleRepository
 import com.comjeonggosi.domain.category.domain.error.CategoryErrorCode
 import com.comjeonggosi.domain.category.domain.repository.CategoryRepository
 import com.comjeonggosi.domain.category.presentation.dto.response.CategoryResponse
@@ -18,17 +20,24 @@ import java.time.Instant
 @Service
 class AdminQuizService(
     private val quizRepository: QuizRepository,
-    private val categoryRepository: CategoryRepository
+    private val categoryRepository: CategoryRepository,
+    private val articleRepository: ArticleRepository
 ) {
     suspend fun createQuiz(request: CreateQuizRequest) {
         val category = categoryRepository.findById(request.categoryId)
             ?: throw CustomException(CategoryErrorCode.CATEGORY_NOT_FOUND)
 
+        val article = request.articleId?.let { id ->
+            articleRepository.findById(id)
+                ?: throw CustomException(ArticleErrorCode.ARTICLE_NOT_FOUND)
+        }
+
         val quiz = QuizDocument(
             content = request.content,
             answer = request.answer,
             options = request.options ?: emptyList(),
-            categoryId = category.id!!
+            categoryId = category.id!!,
+            articleId = article?.id,
         )
 
         quizRepository.save(quiz)
@@ -46,12 +55,23 @@ class AdminQuizService(
     suspend fun updateQuiz(quizId: String, request: UpdateQuizRequest) {
         val quiz = quizRepository.findById(quizId) ?: throw CustomException(QuizErrorCode.QUIZ_NOT_FOUND)
 
+        val category = request.categoryId?.let {
+            categoryRepository.findById(request.categoryId)
+                ?: throw CustomException(CategoryErrorCode.CATEGORY_NOT_FOUND)
+        }
+
+        val article = request.articleId?.let { id ->
+            articleRepository.findById(id)
+                ?: throw CustomException(ArticleErrorCode.ARTICLE_NOT_FOUND)
+        }
+
         quizRepository.save(
             quiz.copy(
                 content = request.content ?: quiz.content,
                 answer = request.answer ?: quiz.answer,
                 options = request.options ?: quiz.options,
-                categoryId = request.categoryId ?: quiz.categoryId
+                categoryId = category?.id ?: quiz.categoryId,
+                articleId = article?.id ?: quiz.articleId,
             )
         )
     }
@@ -79,7 +99,8 @@ class AdminQuizService(
                 id = category.id!!,
                 name = category.name,
                 description = category.description,
-            )
+            ),
+            articleId = this.articleId
         )
     }
 }
