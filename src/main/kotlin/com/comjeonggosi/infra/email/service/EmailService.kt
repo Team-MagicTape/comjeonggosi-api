8082b69cc62e00a991cf72b12e8fe67b4fbe19c5
전민오 <jbj338033@gmail.com>
2025-08-21 20:59:46 +0900

    feat: add alias

--- src/main/kotlin/com/comjeonggosi/infra/email/service/EmailService.kt
+++ src/main/kotlin/com/comjeonggosi/infra/email/service/EmailService.kt
@@ -1,11 +1,11 @@
 package com.comjeonggosi.infra.email.service
 
 import com.comjeonggosi.logger
+import jakarta.mail.internet.InternetAddress
 import jakarta.mail.internet.MimeMessage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.mail.MailProperties
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service
 @Service
 class EmailService(
     private val mailSender: JavaMailSender,
-    private val mailProperties: MailProperties
+    @Value("\${spring.mail.alias}") private val from: String,
 ) {
     private val log = logger()
 
@@ -23,7 +23,7 @@ class EmailService(
                 val message: MimeMessage = mailSender.createMimeMessage()
                 val helper = MimeMessageHelper(message, true, "UTF-8")
                 
-                helper.setFrom(mailProperties.username)
+                helper.setFrom(InternetAddress(from, "Comgo"))
                 helper.setTo(to)
                 helper.setSubject(subject)
                 helper.setText(body, true)
