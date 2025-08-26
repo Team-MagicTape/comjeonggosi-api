3ddb164ff3a8fb5bdfb0039867fee976f803689a
전민오 <jbj338033@gmail.com>
2025-08-26 18:06:36 +0900

    feat: add response status

--- src/main/kotlin/com/comjeonggosi/domain/admin/question/presentation/controller/AdminQuestionController.kt
+++ src/main/kotlin/com/comjeonggosi/domain/admin/question/presentation/controller/AdminQuestionController.kt
@@ -3,17 +3,20 @@ package com.comjeonggosi.domain.admin.question.presentation.controller
 import com.comjeonggosi.domain.admin.question.application.service.AdminQuestionService
 import com.comjeonggosi.domain.admin.question.presentation.dto.request.CreateQuestionRequest
 import com.comjeonggosi.domain.admin.question.presentation.dto.request.UpdateQuestionRequest
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/admin/questions")
 class AdminQuestionController(private val adminQuestionService: AdminQuestionService) {
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     suspend fun createQuestion(@RequestBody request: CreateQuestionRequest) = adminQuestionService.createQuestion(request)
 
     @PatchMapping("/{questionId}")
