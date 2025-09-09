08396a2d98e5d667107d2a931f0cdae2f8588bd7
su080915 <tbr335@naver.com>
2025-09-09 19:25:51 +0900

    feat: article graphql

---
+++ src/main/kotlin/com/comjeonggosi/domain/article/presentation/controller/ArticleGraphqlController.kt
@@ -0,0 +1,17 @@
+package com.comjeonggosi.domain.article.presentation.controller
+
+import com.comjeonggosi.domain.article.application.service.ArticleService
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+
+@Controller
+class ArticleGraphqlController(
+    private val articleService: ArticleService
+) {
+    @QueryMapping
+    fun getArticles(@Argument categoryId: Long?) = articleService.getArticles(categoryId)
+
+    @QueryMapping
+    suspend fun getArticle(@Argument articleId: Long) = articleService.getArticle(articleId)
+}
